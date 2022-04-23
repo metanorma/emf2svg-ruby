f550f72b7283dc9c9322516e29354944eed5d075
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-04-23 22:24:03 +0300

    Expose width and height parameters for target image

--- spec/emf2svg_spec.rb
+++ spec/emf2svg_spec.rb
@@ -7,12 +7,23 @@ RSpec.describe Emf2svg do
 
   let(:example_file) { File.expand_path("examples/image1.emf", __dir__) }
 
-  it "converts from file" do
+  it "converts from file unscaled" do
     expect(described_class.from_file(example_file).size).to eq 39849
   end
 
-  it "converts from string" do
+  it "converts from string unscaled" do
     string = File.read(example_file, mode: "rb")
     expect(described_class.from_binary_string(string).size).to eq 39849
+  end
+
+  it "converts from file scaled" do
+    converted = described_class.from_file(example_file, 800, 600)
+    expect(converted.size).to eq 39607
+  end
+
+  it "converts from string scaled" do
+    string = File.read(example_file, mode: "rb")
+    converted = described_class.from_binary_string(string, 800, 600)
+    expect(converted.size).to eq 39607
   end
 end
